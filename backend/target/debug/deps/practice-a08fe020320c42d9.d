47f2e2f16250302b19cc2b54a8958b6a04d70f41
Heinrich-Dev <boekhoffhenry@gmail.com>
2026-06-28 18:44:23 -0700

    finishing changing things around

---
+++ backend/target/debug/deps/practice-a08fe020320c42d9.d
@@ -0,0 +1,5 @@
+/home/heinrich/rust/practice/target/debug/deps/practice-a08fe020320c42d9.d: src/main.rs
+
+/home/heinrich/rust/practice/target/debug/deps/libpractice-a08fe020320c42d9.rmeta: src/main.rs
+
+src/main.rs:
---
+++ backend/target/debug/practice.d
@@ -0,0 +1,1 @@
+/home/heinrich/rust/rust-vue-app/backend/target/debug/practice: /home/heinrich/rust/rust-vue-app/backend/src/main.rs
